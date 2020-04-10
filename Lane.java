94b64e6f46804e751e258b5e48813791a821aae1
jennyli1299 <jennyli1299@users.noreply.github.com>
2020-04-10 03:00:53 -0400

    Created Class Lane for each lane of Tiles on the map

---
+++ Lane.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class Lane {
+    private Tile[][] lane;
+    private int lanenum;
+
+    public Lane(int n) {
+        lanenum = n;
+        lane = new Tile[8][2];
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 2; c++) {
+                int[] coords = new int[] {lanenum, r, c};
+                Tile t = new Tile(coords);
+                lane[r][c] = t;
+            }
+        }
+        for (int c = 0; c < 2; c++) {
+            lane[0][c].setNexus();
+            lane[7][c].setNexus();
+        }
+    }
+
+    @Override
+    public String toString() {
+        String lanerep = "";
+        for (int r = 0; r < 8; r++) {
+            // Stream<String> stream = lane[r][0].toString().lines();
+            String[] array0 = lane[r][0].toString().lines().toArray(String[]::new);
+            String[] array1 = lane[r][1].toString().lines().toArray(String[]::new);
+            for (int i = 0; i < 3; i++) {
+                lanerep = lanerep + array0[i] + "   " + array1[i] + "   \n";
+            }
+            lanerep = lanerep + "\n";
+        }
+        return lanerep;
+    }
+
+    public static void main(String[] args) {
+        Lane L1 = new Lane(1);
+        System.out.println(L1);
+    }
+}
